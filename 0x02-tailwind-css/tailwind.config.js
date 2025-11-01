df508cde1ed67b6f17f5f4e11e4c30584a6a6c87
PETEDEV09 <peteryeboah09@gmail.com>
2025-11-01 20:07:49 +0100

    move html files back to root

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: [
+    './*.html', // Scans HTML files in the root folder
+    './src/**/*.js', // Correct glob pattern for JS files in src/
+  ],
   theme: {
     extend: {},
   },
